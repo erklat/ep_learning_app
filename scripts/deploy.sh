0a96a5e6eef07dd72f0735d356a0826e2589b105
DavorM <davor_maticevic@epam.com>
2024-12-10 14:57:52 +0100

    adds webhook to deploy to virtual machine

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -7,7 +7,10 @@ command_exists () {
 }
 
 # App directory path variable
-APP_DIR="/home/opc/ep_next_app"
+APP_DIR="/home/opc/ep_learning_app"
+
+# Git Repository URL
+GIT_REPO_URL="git@github.com:erklat/ep_learning_app.git"
 
 # Check if application directory exists, create it if not
 if [ ! -d "$APP_DIR" ]; then
@@ -18,7 +21,7 @@ fi
 # Navigate to the application directory
 cd "$APP_DIR"
 
-# Check for npm
+# Check for npm and Node.js
 if command_exists npm ; then
     echo "npm is installed."
 else
@@ -26,11 +29,26 @@ else
     exit 1
 fi
 
-# Fetch the latest code
-# Assuming the repository has been initialized; you may need to add git clone before this if needed
-git pull origin main
+# Check for Git
+if command_exists git ; then
+    echo "Git is installed."
+else
+    echo "Error: Git is not installed." >&2
+    exit 1
+fi
 
-# Install dependencies
+# Check if a Git repository has been initialized
+if git rev-parse --git-dir > /dev/null 2>&1; then
+    echo "Git repository is initialized."
+    git pull origin main  # Fetch the latest code
+else
+    echo "No Git repository found. Cloning repository..."
+    git clone $GIT_REPO_URL $APP_DIR
+    # Navigate into the directory if git clone does not change directory
+    cd "$APP_DIR"
+fi
+
+# Install project dependencies
 npm install
 
 # Check for PM2 and install if it's not available
@@ -47,5 +65,10 @@ npm run build
 # Start or restart the Next.js application using PM2
 pm2 list | grep "next-app" && pm2 restart next-app || pm2 start npm --name "next-app" -- run start
 
-# Echo success message
-echo "Deployment of Next.js application successful!"
+# Verify the PM2 status and echo success message only when app is running
+if pm2 list | grep -q "next-app.*online"; then
+    echo "Deployment of Next.js application successful!"
+else
+    echo "Failed to start the Next.js application, please check logs."
+    exit 1
+fi
